864c5f314e21e122a513e3697eaa21549a08daa5
Julia Shi <juliajshi@gmail.com>
2025-07-22 12:58:51 -0400

    added scene where birds will randomly appear at 3 designated spots in the experiments folder

--- experiments/julia/cat_resource.gd
+++ experiments/julia/cat_resource.gd
@@ -9,18 +9,9 @@ class_name cat_type
 
 @export var sprite_frames: SpriteFrames
 
-@export var texture: Texture2D
+@export var entry_chance: float
 
 enum types {
 	BLACK,
 	WHITE
 }
-
-func find_appearance():
-	var color: Color 
-	match type: 
-		0: 
-			color = Color(0,0,0,1)
-		1: 
-			color = Color(1,1,1,1)
-	
